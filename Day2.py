a7d8cf7dfaf21d9ebfeab58ab160d58657061e53
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-09 22:26:17 -0500

    day2

---
+++ Day2.py
@@ -0,0 +1,15 @@
+# input()
+    # input take input the user
+# list()
+    #return a list, list("abc") will return ["a","b","c"]
+# dict()
+    # Without arguments, 
+    # The dict() function creates a dictionary.
+    # return the list of names in the current local scope. With an argument, attempt to return a list of valid attributes for that object.
+# sorted()
+    #sorted(iterable[, cmp[, key[, reverse]]])
+    # sorted() is destructive, it mutates the original list, while sort() doesn't
+# open()
+# file()
+# help()
+# dir()

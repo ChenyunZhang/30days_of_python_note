2eaa94c58b69dfd82cca5a4f3fe1a577832e269d
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-09 22:49:42 -0500

    finished day2 level1 exercise

---
+++ day2/Day2__note.py
@@ -0,0 +1,31 @@
+############################### Python Build-in functions
+# input()
+# input take input the user
+# list()
+# return a list, list("abc") will return ["a","b","c"]
+# dict()
+# Without arguments
+# The dict() function creates a dictionary.
+# return the list of names in the current local scope. With an argument, attempt to return a list of valid attributes for that object.
+# sorted()
+# sorted(iterable[, cmp[, key[, reverse]]])
+# sorted() is destructive, it mutates the original list, while sort() doesn't
+# open()
+# file()
+# help()
+# The python help function is used to display the documentation of modules, functions, classes, keywords etc.
+# help(sort)
+# dir()
+# dir() tries to return a valid list of attributes of the object it is called upon.
+# print(dir())
+
+############################### Variable
+# a variable refers to a memory address in which data is stored.
+# a valid variable name must start with a letter or the underscore character.
+# a descriptive variable name is strongly recommended to make the code more readable.
+# variable can be declared in one line:
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+############################### Data Types
+# use type() to check data type
+# list() takes string as its argument

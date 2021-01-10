6ae501ce9cd5295f04419478eefcebc7275ae71e
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-10 18:35:27 -0500

    finished operator note

--- day3_operators/Day3__note.py
+++ day3_operators/Day3__note.py
@@ -13,3 +13,15 @@ print('True or False:', True or False)
 # is not: Returns true if both variables are not the same object(x is not y)
 # in: Returns True if the queried list contains a certain item(x in y)
 # not in: Returns True if the queried list doesn't have a certain item(x in y)
+
+
+# print('1 is 1', 1 is 1)                   # True - because the data values are the same
+# print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
+# print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
+# print('B in Asabeneh', 'B' in 'Asabeneh') # False - there is no uppercase B
+# print('coding' in 'coding for all') # True - because coding for all has the word coding
+# print('a in an:', 'a' in 'an')      # True
+# print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+
+if 1:
+    print("a")

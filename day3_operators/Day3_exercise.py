fcd95a43534c93dacf151b2b5a1d267aee53396b
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-10 19:06:53 -0500

    finished question 8

--- day3_operators/Day3_exercise.py
+++ day3_operators/Day3_exercise.py
@@ -1,37 +1,120 @@
+import math
 # 💻 Exercises - Day 3
-# Declare your age as integer variable
-# Declare your height as a float variable
-# Declare a complex number variable
-# Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+####################################################################
+# 1. Declare your age as integer variable
+
+# Answer:
+# age = 100
+# print("age: ",age)
+
+####################################################################
+# 2. Declare your height as a float variable
+
+# Answer:
+# height = 5.6
+# print("height: ",height)
+
+####################################################################
+# 3. Declare a complex number variable
+
+# Answer:
+# print('Multiplying complex numbers: ',(1+1j) * (1-1j))
+# print(2+1j)
+
+####################################################################
+# 4. Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
 #     Enter base: 20
 #     Enter height: 10
 #     The area of the triangle is 100
-# Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+
+# Answer:
+# base = int(input("Enter base: "))
+# height = int(input("Enter base: "))
+# print("The area of the triangle is "+str(int(base*height*0.5))+".")
+
+####################################################################
+# 5. Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
 # Enter side a: 5
 # Enter side b: 4
 # Enter side c: 3
 # The perimeter of the triangle is 12
-# Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
-# Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
-# Calculate the slope, x-intercept and y-intercept of y = 2x -2
-# Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point (6,10)
-# Compare the slopes in tasks 8 and 9.
+
+# Answer:
+# side_a = int(input("Enter side a: "))
+# side_b = int(input("Enter side b: "))
+# side_c = int(input("Enter side c: "))
+# print("The perimeter of the triangle is "+str(side_a+side_b+side_c)+".")
+
+####################################################################
+# 6.Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+
+# Answer:
+# length = int(input("Enter length: "))
+# width = int(input("Enter width: "))
+# area = length*width
+# perimeter = 2 * (length + width)
+# print("area: "+str(area))
+# print("perimeter: "+str(int(perimeter)))
+
+####################################################################
+# 7. Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+
+# Answer:
+# r = int(input("Enter radius: "))
+# area = round(math.pi * r * r,2)
+# print("area: ",area)
+
+####################################################################
+# 8. Calculate the slope, x-intercept and y-intercept of y = 2x -2
+
+# Answer:
+x = 0
+y = 2*x - 2
+y = 0
+x = (2+y)//2
+
+print(x)
+# 9. Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point (6,10)
+
+# Answer:
+
+
+# 10. Compare the slopes in tasks 8 and 9.
+
+# Answer:
+
 # Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
+
 # Find the length of 'python' and 'jargon' and make a falsy comparison statement.
+
 # Use and operator to check if 'on' is found in both 'python' and 'jargon'
+
 # I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+
 # There is no 'on' in both dragon and python
+
 # Find the length of the text python and convert the value to float and convert it to string
+
 # Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+
 # The floor division of 7 by 3 is equal to the int converted value of 2.7.
+
 # Check if type of '10' is equal to 10
+
 # Check if int('9.8') is equal to 10
+
 # Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+
 # Enter hours: 40
+
 # Enter rate per hour: 28
+
 # Your weekly earning is 1120
+
 # Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume someone lives up to hundred years
+
 # Enter number of years you have lived: 100
+
 # You have lived for 3153600000 seconds.
 # Write a python script that displays the following table
 # 1 1 1 1 1

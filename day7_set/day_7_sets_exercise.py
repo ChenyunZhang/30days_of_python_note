e3ce133f9f050ac8fb9150a77fd88ade831245ec
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-13 19:41:34 -0500

    setup day7

---
+++ day7_set/day_7_sets_exercise.py
@@ -0,0 +1,25 @@
+## 💻 Exercises: Day 7
+
+```py
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+```
+
+1. Find the length of the set it_companies
+2. Add 'Twitter' to it_companies
+3. Insert multiple IT companies at once to the set it_companies
+4. Remove one of the companies from the set it_companies
+5. What is the difference between remove and discard
+6. Join A and B
+7. Find A intersection B
+8. Is A subset of B
+9. Are A and B disjoint sets
+10. Join A with B and B with A
+11. What is the symmetric difference between A and B
+12. Delete the sets completely
+13. Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+14. Explain the difference between the following data types: string, list, tuple and set
+15. _I am a teacher and I love to inspire and teach people._ How many unique words have been used in the sentence? You did not learn loops yet you can do it manually.

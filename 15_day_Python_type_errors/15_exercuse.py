38f757745b3906fb6803a1f4a3e0480d527fe92d
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-22 15:36:00 -0500

    finished day15

---
+++ 15_day_Python_type_errors/15_exercuse.py
@@ -0,0 +1,7 @@
+# ## 💻 Exercises: Day 15
+
+# 1. Open you python interactive shell and try all the examples covered in this section.
+
+# 🎉 CONGRATULATIONS ! 🎉
+
+# [<< Day 14](../14_Day_Higher_order_functions/14_higher_order_functions.md) | [Day 16 >>](../16_Day_Python_date_time/16_python_datetime.md)
---
+++ 15_day_Python_type_errors/15_note.py
@@ -0,0 +1,12 @@
+print "HWLLSLA"
+
+# SyntaxError
+# NameError
+# IndexError: index out of range
+# ModuleNotFoundError
+# AttributeError
+# KeyError
+# TypeError
+# ImportError
+# ValueError
+# ZeroDivisionError

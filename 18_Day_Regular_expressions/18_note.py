3e5675708a5a7cfc0966a0309eb6057c4a1d2b3c
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-26 23:47:40 -0500

    setup dau 19

---
+++ 18_Day_Regular_expressions/18_note.py
@@ -0,0 +1,16 @@
+import re
+# re.match(substring,string,re.I)
+# re.search()
+# re.findall()
+# re.split()
+# re.sub()
+
+# txt = 'I love to teach python and javaScript'
+# match = re.match("I love to teach",txt,re.I)
+# search = re.search("a",txt)
+# print(search)
+# # print(match)
+# span = search.span()
+# start,end = span
+# print(txt[start:end])
+txt = "Python is the most beautiful language that a human being has ever created. I recommend python for a first programming language"
